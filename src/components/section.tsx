45585afe038e2ac183b72fb41f76f1eb7d831602
ShivamBajpai04 <shivambajpai04@gmail.com>
2024-09-11 23:27:24 +0530

    fix deployment

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -5,19 +5,19 @@ interface SectionProps {
   bgColor: string;
 }
 
-export default function Section({
-  id,
-  title,
-  children,
-  bgColor,
-}: SectionProps) {
+export default function Section({ id, title, children }: SectionProps) {
   return (
-    <section id={id} className={`w-full relative min-h-screen bg-[#17191a] text-white`}>
+    <section
+      id={id}
+      className={`w-full relative min-h-screen bg-[#17191a] text-white`}
+    >
       <div className="container mx-auto min-h-screen flex flex-col justify-center align-middle">
         <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl text-center mb-8">
           {title}
         </h2>
-        <div className="flex flex-col align-middle justify-center">{children}</div>
+        <div className="flex flex-col align-middle justify-center">
+          {children}
+        </div>
       </div>
     </section>
   );
